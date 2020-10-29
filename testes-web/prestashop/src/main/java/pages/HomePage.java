fdf6009726c80b01dd14b1c87e78bd683ef646e1
Kleber Nascimento <klebernascimento@outlook.com.br>
2020-10-29 14:14:52 -0300

    Testar o Fluxo Padrão - Parte 1

--- testes-web/prestashop/src/main/java/pages/HomePage.java
+++ testes-web/prestashop/src/main/java/pages/HomePage.java
@@ -7,14 +7,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+
 public class HomePage {
 	
 	private WebDriver driver;
 	
 	List<WebElement> listaProdutos = new ArrayList();
 	
+	/*
+	 * Recuperação de itens da pagina
+	 */
 	private By produtos = By.className("product-description");
 	private By textoProdutosNoCarrinho = By.className("cart-products-count");
+	private By descricosDosProdutos = By.cssSelector(".product-description a");
+	private By precoDosProdutos = By.cssSelector(".product-price-and-shipping .price");
 	
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
@@ -36,6 +42,21 @@ public class HomePage {
 		
 		int qtdProdutosNoCarrinho = Integer.parseInt(quantidadeProdutos);
 		return qtdProdutosNoCarrinho;
+	}
+	
+	public String obterNomeProduto(int indice) {
+		String descricao = driver.findElements(descricosDosProdutos).get(indice).getText();
+		return descricao;
+	}
+	
+	public String obterPrecoProduto(int indice) {
+		String preco = driver.findElements(precoDosProdutos).get(indice).getText();
+		return preco;
+	}
+
+	public ProdutoPage clicarProduto(int indice) {
+		driver.findElements(descricosDosProdutos).get(indice).click();
+		return new ProdutoPage(driver);
 	}
 	
 
